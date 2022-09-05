5878e31b9335ee65905b89b6ef3a91ea94251f57
feliperaindo <felipe.raindo@gmail.com>
2022-09-05 15:13:21 -0300

    refatora função para geração de cores

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-function genetareRandomColorPalette() {
+function checkColorWhite(element) {
+  if (element.style.backgroundColor === 'rgb(255, 255, 255)'
+        || element.style.backgroundColor === '#ffffff'
+        || element.style.backgroundColor === 'white') {
+    return false;
+  }
+}
+function checkRepetitionColor(element) {
   const getElement = document.querySelectorAll('.color');
 
-  for (let index = 0; index < 4; index += 1) {
-    if (index > 0) {
-      const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-      getElement[index].style.backgroundColor = randomColor;
+  for (let index = 0; index < getElement.length; index += 1) {
+    if (element.style.backgroundColor === getElement[index].style.backgroundColor) {
+      return false;
     }
   }
 }
-genetareRandomColorPalette();
+function randomPaletteColor() {
+  const getElement = document.querySelectorAll('.color');
+
+  for (let index = 1; index < 4; index += 1) {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    getElement[index].style.backgroundColor = randomColor;
+    checkColorWhite(getElement[index]);
+    checkRepetitionColor(getElement[index]);
+    if (checkColorWhite === false || checkRepetitionColor === false) {
+      index = 1;
+    }
+  }
+}
+randomPaletteColor();
 
 // function createHightPixelBoard() {
 //  let element = document.getElementById('pixel-board');
