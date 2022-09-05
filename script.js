f13edd874c308587c6caa393b15cca39ae6a834a
feliperaindo <felipe.raindo@gmail.com>
2022-09-05 14:42:57 -0300

    resolve requisito 2

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-document.body.onload = createHightPixelBoard();
+function genetareRandomColorPalette() {
+  const getElement = document.querySelectorAll('.color');
 
-function createHightPixelBoard() {
- let element = document.getElementById('pixel-board');
- let elementCreator = document.createElement('div').
-
-    for (let i = 0; i < 5; i += 1) {
-    element.appendChild(elementCreator);
-    console.log element;
+  for (let index = 0; index < 4; index += 1) {
+    if (index > 0) {
+      const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+      getElement[index].style.backgroundColor = randomColor;
     }
+  }
 }
+genetareRandomColorPalette();
+
+// function createHightPixelBoard() {
+//  let element = document.getElementById('pixel-board');
+//  let elementCreator = document.createElement('div').
+
+//     for (let i = 0; i < 5; i += 1) {
+//     element.appendChild(elementCreator);
+//     console.log element;
+//     }
+// }
---
+++ teste.js
@@ -0,0 +1,5 @@
+function genetareRandomColorPalette() {
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  console.log(randomColor);
+}
+genetareRandomColorPalette();
